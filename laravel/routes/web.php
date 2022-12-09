8de2d612f136c2c2b41ce60fa9ca0b9678b0f511
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-12-09 23:09:55 +0100

    update

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,10 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/event', function () {
+    return view('event');
 });
