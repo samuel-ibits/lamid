c326906b41ae0a07247b2ba446fbc76885fabfda
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-12-12 10:54:08 +0100

    update resurces

---
+++ laravel/resources/asset/Componets/menu/menu.js
@@ -0,0 +1,11 @@
+$(".menu-icon").click(function () {
+  $("#menuList").toggle('slow');
+});
+$("#slider").on("input change", (e)=>{
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $('.foreground-img').css('width', `${sliderPos}%`)
+  // Update the position of the slider button
+  $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
+});
+
